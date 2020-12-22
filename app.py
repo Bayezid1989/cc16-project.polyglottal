55d995428af1e48c98c90d9f6e1131626b0395d5
bayezid1989 <bayezid1989@live.jp>
2020-12-22 20:10:01 +0900

    Added absence

--- app.py
+++ app.py
@@ -24,7 +24,16 @@ line_bot_api = LineBotApi(config._LINE_TOKEN)
 handler = WebhookHandler(config._LINE_SECRET)
 
 
-@app.route("/callback", methods=['POST'])
+rich_menu = ["欠席 / Absence", "遅刻 / Tardiness", "早退 / Leave early", "連絡、質問 / Contact, Question", "回答、提出 / Answer, Submit", "その他 / Others"
+             ]
+
+with open('language/japanese.json') as japanese:
+    japanese_words = json.load(japanese)
+with open('language/english.json') as english:
+    english_words = json.load(english)
+
+
+@ app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
@@ -43,20 +52,20 @@ def callback():
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
+@ handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    print("event", event)
+    print("message event", event)
     user_id = event.source.user_id
-    complete_key = client.key("UserKind", user_id)
-    user = client.get(complete_key)
+    user_key = client.key("UserKind", user_id)
+    user = client.get(user_key)
+    action_key = client.key("ActionKind", user_id)
+    action = client.get(action_key)
     if (user is not None) and (user["isEnglish"] == True):
-        with open('language/english.json') as english:
-            words = json.load(english)
+        words = english_words
     else:
-        with open('language/japanese.json') as japanese:
-            words = json.load(japanese)
+        words = japanese_words
     if user is None:
-        user = datastore.Entity(key=complete_key)
+        user = datastore.Entity(key=user_key)
         user.update({
             "isEnglish": False,
             "child_name": "",
@@ -78,8 +87,7 @@ def handle_message(event):
             with client.transaction():
                 user["isEnglish"] = True
                 client.put(user)
-            with open('language/english.json') as english:
-                words = json.load(english)
+            words = english_words
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=words["childName"]))
@@ -151,18 +159,173 @@ def handle_message(event):
             client.put(user)
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=words["registerCompleted"]))
+            TextSendMessage(
+                text=words["registerCompleted"],
+                quick_reply=QuickReply(
+                    items=[
+                        QuickReplyButton(
+                            action=MessageAction(
+                                label=words["absence"], text=words["absence"]),
+                            # action=DatetimePickerAction(
+                            #     label=words["absence"], data="absence_date_postback", mode="date")
+                        ),
+                        QuickReplyButton(
+                            action=MessageAction(
+                                label=words["tardiness"], text=words["tardiness"]),
+                        ),
+                        QuickReplyButton(
+                            action=MessageAction(
+                                label=words["leave_early"], text=words["leave_early"]),
+                        ),
+                        QuickReplyButton(
+                            action=MessageAction(
+                                label=words["contact_question"], text=words["contact_question"]),
+                        ),
+                        QuickReplyButton(
+                            action=MessageAction(
+                                label=words["answer_submit"], text=words["answer_submit"]),
+                        ),
+                        QuickReplyButton(
+                            action=MessageAction(
+                                label=words["others"], text=words["others"]),
+                        ),
+                    ])))
+
+    elif (action is None) and (event.message.text in [words["absence"], words["tardiness"], words["leave_early"]]):
+        if event.message.text == words["absence"]:
+            category = "absence"
+        elif event.message.text == words["tardiness"]:
+            category = "tardiness"
+        else:
+            category = "leave_early"
+        action_key = client.key("ActionKind", user_id)
+        action = datastore.Entity(key=action_key)
+        action.update(
+            {
+                "category": category,
+                "when": "",
+                "reason": "",
+            }
+        )
+        client.put(action)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=words["askReason"]))
+
+    elif (action is not None) and (action["reason"] == ""):
+        with client.transaction():
+            action["reason"] = event.message.text
+            client.put(action)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(
+                text=words["proceedAbsence"],
+                quick_reply=QuickReply(
+                    items=[
+                        QuickReplyButton(
+                            action=PostbackAction(
+                                label=words["today"], data='absence_postback_today', displayText=words["today"]),
+                        ),
+                        QuickReplyButton(
+                            action=DatetimePickerAction(
+                                label=words["chooseDate"], data="absence_postback_date", mode="date")
+                        ),
+                        QuickReplyButton(
+                            action=MessageAction(
+                                label=words["cancel"], text=words["cancel"]),
+                        ),
+                    ])))
+    elif (action is not None) and (event.message.text == words["cancel"]):
+        client.delete(action_key)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=words["cancelDone"]))
     elif event.message.text == "Delete":
-        client.delete(complete_key)
+        client.delete(user_key)
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="User deleted"))
     else:
-        message = "I don't know."
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=message))
+            TextSendMessage(
+                text=words["feelFree"],
+                quick_reply=QuickReply(
+                    items=[
+                        QuickReplyButton(
+                            action=MessageAction(
+                                label=words["absence"], text=words["absence"]),
+                        ),
+                        QuickReplyButton(
+                            action=MessageAction(
+                                label=words["tardiness"], text=words["tardiness"]),
+                        ),
+                        QuickReplyButton(
+                            action=MessageAction(
+                                label=words["leave_early"], text=words["leave_early"]),
+                        ),
+                        QuickReplyButton(
+                            action=MessageAction(
+                                label=words["contact_question"], text=words["contact_question"]),
+                        ),
+                        QuickReplyButton(
+                            action=MessageAction(
+                                label=words["answer_submit"], text=words["answer_submit"]),
+                        ),
+                        QuickReplyButton(
+                            action=MessageAction(
+                                label=words["others"], text=words["others"]),
+                        ),
+                    ])))
+
+
+@ handler.add(PostbackEvent)
+def handle_postback(event):
+    print("postback event", event)
+    user_id = event.source.user_id
+    user_key = client.key("UserKind", user_id)
+    user = client.get(user_key)
+    action_key = client.key("ActionKind", user_id)
+    action = client.get(action_key)
+    if (user is not None) and (user["isEnglish"] == True):
+        words = english_words
+    else:
+        words = japanese_words
+    if event.postback.data[0:17] == "absence_postback_":
+        if event.postback.data == "absence_postback_today":
+            print("not yet done")
+        elif event.postback.data == "absence_postback_date":
+            when = event.postback.params['date']
+        with client.transaction():
+            action["when"] = when
+            client.put(action)
+        absenceConfirm = words["absenceConfirm"]
+        dateTimeKey = words["dateTime"]
+        whenValue = action["when"]
+        reasonKey = words["reason"]
+        reasonValue = action["reason"]
+        confirm_template = ConfirmTemplate(text=f"{absenceConfirm} --- {dateTimeKey}: {whenValue}, {reasonKey}: {reasonValue}", actions=[
+            PostbackAction(
+                label=words["yes"], data="absence_execte_postback", displayText=words["yes"]),
+            MessageAction(
+                label=words["cancel"], text=words["cancel"]),
+        ])
+        template_message = TemplateSendMessage(
+            alt_text='Confirm absence', template=confirm_template)
+        line_bot_api.reply_message(event.reply_token, template_message)
+
+    elif event.postback.data == "absence_execte_postback":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="absence_execte_postback"))
+        # elif event.postback.data == 'datetime_postback':
+        #     line_bot_api.reply_message(
+        #         event.reply_token, TextSendMessage(text=event.postback.params['datetime']))
+        # elif event.postback.data == 'date_postback':
+        #     line_bot_api.reply_message(
+        #         event.reply_token, TextSendMessage(text=event.postback.params['date']))
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0",
+            port=int(os.environ.get("PORT", 8080)))
